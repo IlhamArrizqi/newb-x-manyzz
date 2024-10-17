a7b193f6ebd9fe6d6ea24c77a9324f79e5e87018
IlhamArrizqi <itszmanyzz@gmail.com>
2024-10-17 16:03:03 +0700

    back to back

--- src/newb/functions/clouds.h
+++ src/newb/functions/clouds.h
@@ -41,8 +41,63 @@ vec4 renderCloudsSimple(nl_skycolor skycol, vec3 pos, highp float t, float rain)
 }
 
 // rounded clouds
+#ifdef NL_CLOUD2_REALISTIC
+float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
 
+float noise(vec3 p){
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (2.0 - 1.0 * d);
+
+    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
+
+    vec4 c = k2 + a.zzzz;
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
+
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+    return o4.y * d.y + o4.x * (1.0 - d.y);
+}
+#endif
+
+#ifdef NL_CLOUD2_SMOOTH
+float noise(vec2 p){
+  vec2 p0 = floor(p);
+  vec2 u = p-p0;
+
+  u *= u*(2.0-1.0*u);
+  vec2 v = 1.0 - u;
+
+  float c1 = rand(p0);
+  float c2 = rand(p0+vec2(1.0,0.0));
+  float c3 = rand(p0+vec2(0.0,1.0));
+  float c4 = rand(p0+vec2(1.0,1.0));
+  float c5 = rand(p0+vec2(1.0,0.0));
+  float c6 = rand(p0+vec2(1.0,0.0));
+
+  float n = v.y*mix(c1,c2,u.x+c6) + u.y*(c3*v.x+c4+c5*u.x);
+  return n;
+}
+#endif
 // rounded clouds 3D density map
+  #ifdef NL_CLOUD2_REALISTIC
+  pos.xyz += 0.0*noise(pos.xyz);
+  pos.xz += 0.7*noise(7.0*pos.xyz);
+#endif
+
+  #ifdef NL_CLOUD2_SMOOTH
+  pos.xz += 1.99*noise(99.99*pos.xz);
+#endif
+
 float cloudDf(vec3 pos, float rain) {
   vec2 p0 = floor(pos.xz);
   vec2 u = smoothstep(0.999*NL_CLOUD2_SHAPE, 1.0, pos.xz-p0);
