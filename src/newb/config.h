8a3e51f873b28ee24a7f32b13e58d0bfdd770402
IlhamArrizqi <itszmanyzz@gmail.com>
2026-07-08 12:54:20 +0700

    subtle

--- src/newb/config.h
+++ src/newb/config.h
@@ -61,7 +61,7 @@
 
 /* Fog */
 #define NL_FOG 1.0                // [toggle] 0.1 subtle ~ 1.0 blend with sky completely
-#define NL_MIST_DENSITY 100.0      // 0.0 no mist ~ 1.0 misty
+#define NL_MIST_DENSITY 0.18      // 0.0 no mist ~ 1.0 misty
 #define NL_RAIN_MIST_OPACITY 0.16 // [toggle] 0.04 very subtle ~ 0.5 thick rain mist blow
 #define NL_CLOUDY_FOG 0.6         // [toggle] 0.0 subtle - 0.8 dense fog clouds
 
